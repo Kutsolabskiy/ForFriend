996cdbb7a114b93bc8da2f4a4d420fb735602b67
Maksym Skrypchenko <maxvallskr@gmail.com>
2020-03-02 21:27:42 +0200

    Max pofixel

--- src/main/java/test/IntegerRandom.java
+++ src/main/java/test/IntegerRandom.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IntegerRandom {
     static WebDriver driver;
@@ -15,12 +17,23 @@ public class IntegerRandom {
     static int min = 99;
     static int max = 200;
     static int col = 5;
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.random.org/integers/");
 
+    public static void main(String[] args) {
+        testCheckColumn();
+    }
+
+    private static void testCheckColumn() {
+        List<List<Integer>> input = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(5, 6, 7)
+        );
+        checkColumn(input);
+    }
+
+    private static void test() {
+        setupDriver();
+        driver.get("https://www.random.org/integers/");
         System.out.println(driver.getCurrentUrl());
 
         driver.findElement(By.cssSelector("[name='num']")).clear();
@@ -36,73 +49,68 @@ public class IntegerRandom {
         String colResult = driver.findElement(By.cssSelector(".data")).getText();
         driver.quit();
 
-        String [] listS = colResult.split("\n");
-
-        List<List<String>> list = new ArrayList<>();
-        for(String s : listS){
-            list.add(Arrays.asList(s.split(" ")));
-        }
-
         List<List<Integer>> listInt = new ArrayList<>();
-        for(int i = 0; i < list.size(); i++){
-            ArrayList<Integer> listIntsmall = new ArrayList<>();
-            for(int j = 0; j < list.get(i).size(); j++){
-                int x = Integer.parseInt(list.get(i).get(j));
-                listIntsmall.add(x);
-        }
-            listInt.add(listIntsmall);
-        }
-
-        checkNum(list);
-        checkMinMax(listInt);
-        checkColum(listInt);
-
-    }
-    public static void checkNum(List<List<String>> list){
-        int count = 0;
-        for(int i = 0; i < list.size(); i++){
-             for(int j = 0; j < list.get(i).size(); j++){
-            count ++;
+        for (String rowAsString : colResult.split("\n")) {
+            List<Integer> rowNumbers = new ArrayList<>();
+            for (String numberCell : rowAsString.split(" ")) {
+                rowNumbers.add(Integer.parseInt(numberCell));
             }
+            listInt.add(rowNumbers);
         }
-        if(count == num){
+
+        checkNum(listInt);
+        checkMinMax(listInt);
+        checkColumn(listInt);
+    }
+
+    private static void submitForm() {
+        // todo
+    }
+
+    private static void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public static <T> void checkNum(List<List<T>> list) {
+        int count = 0;
+        for (List<T> strings : list) {
+            count += strings.size();
+        }
+        if (count == num) {
             System.out.println("Count test is correct");
-        }else {
+        } else {
             System.out.println("error404");
         }
     }
-    public static void checkMinMax(List<List<Integer>> list){
-        int error = 0;
-        for(int i = 0; i < list.size(); i++){
-            for(int j = 0; j < list.get(i).size(); j++){
-                if(list.get(i).get(j) <= max || list.get(i).get(j) >= min){
-                    continue;
-                }else{
-                    error = 1;
-                    break;
+
+    public static void checkMinMax(List<List<Integer>> list) {
+        for (List<Integer> integers : list) {
+            for (Integer integer : integers) {
+                if (!(integer <= max || integer >= min)) {
+                    System.out.println("сожги себе ебало гнида криворукая");
+                    return;
                 }
             }
         }
-        if(error == 0){
-            System.out.println("Min/Max test is correct");
-        }else{
-            System.out.println("сожги себе ебало гнида криворукая");
-        }
+        System.out.println("Min/Max test is correct");
     }
-    public static void checkColum (List<List<Integer>> list) {
+
+    public static void checkColumn(List<List<Integer>> list) {
         int error = 0;
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).size() == col){
+            if (list.get(i).size() == col) {
                 continue;
-            }else if(list.get(i).size() == num % col ){
+            } else if (list.get(i).size() == num % col) {
                 continue;
-            }else{
+            } else {
                 error++;
             }
         }
-        if(error == 0){
+        if (error == 0) {
             System.out.println("Check Colum test is correct");
-        }else {
+        } else {
             System.out.println("shit");
         }
     }

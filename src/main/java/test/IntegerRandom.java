917fe533b46ad0903f29f6b56128bd2ebaed217c
Kutsolabskiy <Kutsolabskiytattoo@gmail.com>
2020-03-04 16:45:22 +0700

    %commit_message%

--- src/main/java/test/IntegerRandom.java
+++ src/main/java/test/IntegerRandom.java
@@ -4,34 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class IntegerRandom {
     static WebDriver driver;
-    static int num = 33;
+    static int num = 24;
     static int min = 99;
     static int max = 200;
-    static int col = 5;
+    static int col = 3;
 
     public static void main(String[] args) {
-        testCheckColumn();
+        submitForm();
     }
 
-    private static void testCheckColumn() {
-        List<List<Integer>> input = Arrays.asList(
-                Arrays.asList(1, 2, 3, 4, 5),
-                Arrays.asList(4, 5, 6),
-                Arrays.asList(5, 6, 7)
-        );
-        checkColumn(input);
-    }
-
-    private static void test() {
+    private static void submitForm() {
         setupDriver();
         driver.get("https://www.random.org/integers/");
         System.out.println(driver.getCurrentUrl());
@@ -45,7 +38,16 @@ public class IntegerRandom {
         driver.findElement(By.cssSelector("[name='col']")).clear();
         driver.findElement(By.cssSelector("[name='col']")).sendKeys(String.valueOf(col));
         driver.findElement(By.cssSelector("[value='Get Numbers']")).click();
+        getResult();
+    }
 
+    private static void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    private static void getResult() {
         String colResult = driver.findElement(By.cssSelector(".data")).getText();
         driver.quit();
 
@@ -60,32 +62,9 @@ public class IntegerRandom {
 
         checkNum(listInt);
         checkMinMax(listInt);
-        checkColumn(listInt);
     }
 
-    private static void submitForm() {
-        // todo
-    }
-
-    private static void setupDriver() {
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    public static <T> void checkNum(List<List<T>> list) {
-        int count = 0;
-        for (List<T> strings : list) {
-            count += strings.size();
-        }
-        if (count == num) {
-            System.out.println("Count test is correct");
-        } else {
-            System.out.println("error404");
-        }
-    }
-
-    public static void checkMinMax(List<List<Integer>> list) {
+    private static void checkMinMax(List<List<Integer>> list) {
         for (List<Integer> integers : list) {
             for (Integer integer : integers) {
                 if (!(integer <= max || integer >= min)) {
@@ -97,22 +76,53 @@ public class IntegerRandom {
         System.out.println("Min/Max test is correct");
     }
 
-    public static void checkColumn(List<List<Integer>> list) {
-        int error = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).size() == col) {
-                continue;
-            } else if (list.get(i).size() == num % col) {
-                continue;
-            } else {
-                error++;
+    private static <T> void checkNum2(List<List<T>> list) {
+        int count = 0;
+        for (List<T> strings : list) {
+            count += strings.size();
+        }
+        if (count == num) {
+            System.out.println("Count test is correct");
+            checkColumn(list);
+        } else {
+            System.out.println("Count test is false");
+        }
+    }
+
+    private static <T> void checkNum(List<List<T>> list) {
+        List<T> numList = list.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        if(numList.size() == num){
+            System.out.println("Num test is true");
+            checkColumn(list);
+        }else{
+            System.out.println("Num test is false");
+        }
+
+    }
+
+    private static <T> void checkColumn(List<List<T>> list) {
+        for (int row = 0; row < list.size() - 1; row++) {
+            if (!(list.get(row).size() == col)) {
+                System.out.println("Column is false");
+                return;
             }
         }
-        if (error == 0) {
-            System.out.println("Check Colum test is correct");
-        } else {
-            System.out.println("shit");
-        }
+        System.out.println("Column is " + (list.get(list.size()-1).size() <= col));
+    }
+
+    private static <T> void checkColumnStream(List<List<T>> list){
+    //Здесь было то самое чувство когда Нихуя не вышло
+    }
+
+    private static void testCheckColumn() {
+        List<List<Integer>> input = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5),
+                Arrays.asList(4, 5, 6, 4, 5),
+                Arrays.asList(4, 5, 6, 4),
+                Arrays.asList(4, 5, 6, 4, 4),
+                Arrays.asList(4, 5, 6, 4, 5)
+        );
+        checkNum(input);
     }
 }
 
--- src/main/java/test/PasswordSimpleTryStream.java
+++ src/main/java/test/PasswordSimpleTryStream.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
